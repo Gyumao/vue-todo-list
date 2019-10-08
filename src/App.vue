260d563bb8a3f5d99f50a43307b7053d0b705b38
Richard Gobert <dev.richard.g@gmail.com>
2019-10-09 00:01:21 +0200

    modification Index and app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container">
-    <h1 class="title is-1">Vue Todo List</h1>
+    <h1 class="title is-1">VUE TODO LIST</h1>
     <TodoList />
   </div>
 </template>
@@ -17,6 +17,9 @@ export default {
 </script>
 
 <style lang="scss">
+body{
+  text-align: center;
+}
 .container {
   margin: 100px 60px 0 100px};
 #app {
@@ -24,15 +27,29 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: black;
+  color:red;
   margin-top: 60px;
+  font-size: 3rem;
+  font-weight: bold;
 }
 .title{
-  color: blue;
+  color: cyan ;
+  font-weight: bold;
+  font-size: 4rem;
 }
 .input{
   border: 10px inset black;
-  box-shadow: 3px 3px 3px yellow, 0 0 2em red, 0 0 2em red;
-  margin-bottom: 3rem;
-}
+  box-shadow: 3px 3px 3px yellow, 0 0 3em red, 0 0 3em red;
+  margin-bottom: 2rem;
+  text-align: center;
+  color: whitesmoke;
+  font-weight: bold;
+  position: relative;
+}  
+  ::placeholder
+  {
+    font-size: 1.5rem;
+    font-weight: bold;
+    text-shadow: 2px 2px whitesmoke;
+  }
 </style>
