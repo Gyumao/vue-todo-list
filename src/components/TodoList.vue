3b6f6c4d130ab4b92ad578d9f9b9eda8f488e41d
Richard Gobert <dev.richard.g@gmail.com>
2019-10-14 00:07:42 +0200

    add pages deploy.sh and vue.config.js, modification gitignore

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,40 +1,36 @@
 <template>
-    <div>
-        <TodoInputText v-model="newTodoText" @keydown.enter="addTodo"/>
-        <TodoListItem :todos="todos" />
-    </div>
+  <div>
+    <TodoInputText v-model="newTodoText" @keydown.enter="addTodo" />
+    <TodoListItem :todos="todos" />
+  </div>
 </template>
 
 <script>
-import TodoInputText from '@/components/TodoInputText.vue';
-import TodoListItem from '@/components/TodoListItem.vue';
-
+import TodoInputText from "@/components/TodoInputText.vue";
+import TodoListItem from "@/components/TodoListItem.vue";
 
 export default {
-    components:{
-        TodoInputText,
-        TodoListItem,
-
-    },
-    data(){
-        return{
-          todos: ["pomme", "poire", "chocolat"],
-          newTodoText: "",
-        };
-        
-    },
-    methods: {
-        addTodo(){
-            const trimmedText = this.newTodoText.trim();
-            if (trimmedText) {
-            this.todos.push(trimmedText);
-            this.newTodoText = "";
-            }
-        }
+  components: {
+    TodoInputText,
+    TodoListItem
+  },
+  data() {
+    return {
+      todos: ["pomme", "poire", "chocolat"],
+      newTodoText: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      const trimmedText = this.newTodoText.trim();
+      if (trimmedText) {
+        this.todos.push(trimmedText);
+        this.newTodoText = "";
+      }
     }
+  }
 };
 </script>
 
 <style>
-
 </style>
